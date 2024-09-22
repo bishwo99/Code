ada8d9eacaea0c7489f84b7da55cd094ade6ab91
bishwo99 <112264657+bishwo99@users.noreply.github.com>
2024-09-22 12:54:56 +0600

    Update PrintAllDivisor.cpp

--- PrintAllDivisor.cpp
+++ PrintAllDivisor.cpp
@@ -1,1 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+void Divisor(int n)
+{
+    vector<int> ls;
+    for(int i=1;i<=sqrt(n);i++)
+    {
+        if(n%i==0){
+              ls.push_back(i);
+        }
+      
+        if((n/i)!=i)
+        {
+            ls.push_back(n/i);
+        }
+        
+    }
+    sort(ls.begin(),ls.end());
+    for(auto it : ls ) cout<<it<<" ";
+}
 
+int main()
+{
+   int a;
+   cin>>a;
+   Divisor(a);
+
+     
+}
