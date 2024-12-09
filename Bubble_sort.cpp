266ed63b7f09c31db1e9eca3369b882925137507
bishwo99 <112264657+bishwo99@users.noreply.github.com>
2024-12-09 23:33:57 +0600

    Bubble_sort.cpp

--- Bubble_sort.cpp
+++ Bubble_sort.cpp
@@ -4,13 +4,17 @@ using namespace std;
 void bubble_sort(int arr[], int n)
 {
     for(int i=n-1;i>=1;i--){
+        int didSwap=0;
         for(int j=0;j<=i-1;j++){
             if(arr[j] > arr[j+1]){
                 int tmp=arr[j+1];
                 arr[j+1]=arr[j];
                 arr[j]=tmp;
+                didSwap=1;
             }
         }
+        if(didSwap=0){
+            break;
     }
 
      }
