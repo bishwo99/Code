b99e7f6a87336750659bd1fc4f5b44cbd4284d53
bishwo99 <112264657+bishwo99@users.noreply.github.com>
2024-12-20 23:19:17 +0600

    Add files via upload

---
+++ 26 Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>> arr[i];
+    }
+  int k=0;
+  for(int i=1;i<arr.size();i++)
+  {
+      if(arr[i]!=arr[k])
+      {
+          arr[k+1] = arr[i];
+          k++;
+
+      }
+
+  }
+
+    for(int i=0;i<(k+1);i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+}
